7b9b6344ad69d36b2c899e02823eac7b10b5e67b
Henrique Miraldo <rickmiraldo@gmail.com>
2018-12-14 10:48:31 +0000

    Projeto Employee-OutsourcedEmployee

--- CursoCSharpUdemy/Program.cs
+++ CursoCSharpUdemy/Program.cs
@@ -9,31 +9,37 @@ namespace CursoCSharpUdemy
     {
         static void Main(string[] args)
         {
-            Account acc = new Account(1001, "Alex", 0.0);
-            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+            List<Employee> employees = new List<Employee>();
 
-            // Upcasting
-            Account acc1 = bacc;
-            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+            Console.Write("Enter the number of employees: ");
+            int nEmp = int.Parse(Console.ReadLine());
 
-            // Downcasting
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-            acc4.Loan(100.0);
-            // BusinessAccount acc5 = (BusinessAccount)acc3; -- Erro em tempo de execução!
-            if (acc3 is BusinessAccount)
+            for (int i = 1; i <= nEmp; i++)
             {
-                // BusinessAccount acc5 = (BusinessAccount)acc3;
-                BusinessAccount acc5 = acc3 as BusinessAccount; // Sintaxe alternativa
-                acc5.Loan(200.0);
-                Console.WriteLine("Loan!");
+                Console.WriteLine("\nEmployee #" + i + " data:");
+                Console.Write("Outsourced? (y/n): ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double adCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employees.Add(new OutsourcedEmployee(name, hours, value, adCharge));
+                } else
+                {
+                    employees.Add(new Employee(name, hours, value));
+                }
             }
-            if (acc3 is SavingsAccount)
+
+            Console.WriteLine("\nPAYMENTS:");
+            foreach (Employee e in employees)
             {
-                // SavingsAccount acc5 = (SavingsAccount)acc3;
-                SavingsAccount acc5 = acc3 as SavingsAccount; // Sintaxe alternativa
-                acc5.UpdateBalance();
-                Console.WriteLine("Update!");
+                Console.WriteLine(e.Name + " - R$ " + e.Payment().ToString("F2", CultureInfo.InvariantCulture));
             }
         }
     }
