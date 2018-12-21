4296ae00451ce6aab3c05e8cb739828bd6adcaf4
Henrique Miraldo <rickmiraldo@gmail.com>
2018-12-21 14:03:28 +0000

    LINQ com Lambda

--- CursoCSharpUdemy/Program.cs
+++ CursoCSharpUdemy/Program.cs
@@ -3,44 +3,96 @@ using System.Globalization;
 using System.Collections.Generic;
 using System.IO;
 using CursoCSharpUdemy.Entities;
-using CursoCSharpUdemy.Entities.Exceptions;
+using System.Linq;
 
 namespace CursoCSharpUdemy
 {
     class Program
     {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach (T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Enter account data:");
-                Console.Write("Number: ");
-                int number = int.Parse(Console.ReadLine());
-                Console.Write("Holder: ");
-                string name = Console.ReadLine();
-                Console.Write("Initial balance: ");
-                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Withdraw limit: ");
-                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
 
-                Account acc = new Account(number, name, balance, limit);
+            List<Product> products = new List<Product>() {
+                new Product() { Id = 1, Name = "Computer", Price = 1100.00, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.00, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.00, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.00, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.00, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.00, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.00, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.00, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.00, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.00, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.00, Category = c1 }
+            };
 
-                Console.Write("\nEnter amount for withdraw: ");
-                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                acc.Withdraw(amount);
-                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
-            }
-            catch (DomainException e)
+            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            Print("TIER 1 AND PRICE < 900:", r1);
+
+            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
+
+            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECTS:", r3);
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+            var r5 = r4.Skip(2).Take(4);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+
+            var r6 = products.FirstOrDefault();
+            Console.WriteLine("FirstOrDefault test1: " + r6);
+
+            var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            Console.WriteLine("FirstOrDefault test2: ", r7 + "\n");
+
+            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
+            Console.WriteLine("\nSingleOrDefault test1: " + r8);
+
+            var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
+            Console.WriteLine("SingleOrDefault test2: " + r9 + "\n");
+
+            var r10 = products.Max(p => p.Price);
+            Console.WriteLine("Max price: " + r10);
+
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine("Min price: " + r11 + "\n");
+
+            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Category 1 Sum Prices: " + r12);
+
+            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+            Console.WriteLine("Category 1 Average Prices: " + r13);
+
+            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty().Average();
+            Console.WriteLine("Category 5 Average Prices: " + r14);
+
+            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
+            Console.WriteLine("Category 1 Aggregate sum: " + r15 + "\n");
+
+            var r16 = products.GroupBy(p => p.Category);
+            foreach (IGrouping<Category, Product> group in r16)
             {
-                Console.WriteLine("Withdraw error: " + e.Message);
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Format error: " + e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected error: " + e.Message);
+                Console.WriteLine("Category " + group.Key.Name + ":");
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
             }
         }
     }
