720c7e141c4c0b093deb235d57b997103ca50d98
Henrique Miraldo <rickmiraldo@gmail.com>
2018-12-21 15:07:56 +0000

    Projeto Employee com Linq

--- CursoCSharpUdemy/Program.cs
+++ CursoCSharpUdemy/Program.cs
@@ -9,130 +9,43 @@ namespace CursoCSharpUdemy
 {
     class Program
     {
-        static void Print<T>(string message, IEnumerable<T> collection)
-        {
-            Console.WriteLine(message);
-            foreach (T obj in collection)
-            {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine();
-        }
-
         static void Main(string[] args)
         {
-            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
-            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
-            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+            Console.Write("Enter full file path: ");
+            string filePath = Console.ReadLine();
 
-            List<Product> products = new List<Product>() {
-                new Product() { Id = 1, Name = "Computer", Price = 1100.00, Category = c2 },
-                new Product() { Id = 2, Name = "Hammer", Price = 90.00, Category = c1 },
-                new Product() { Id = 3, Name = "TV", Price = 1700.00, Category = c3 },
-                new Product() { Id = 4, Name = "Notebook", Price = 1300.00, Category = c2 },
-                new Product() { Id = 5, Name = "Saw", Price = 80.00, Category = c1 },
-                new Product() { Id = 6, Name = "Tablet", Price = 700.00, Category = c2 },
-                new Product() { Id = 7, Name = "Camera", Price = 700.00, Category = c3 },
-                new Product() { Id = 8, Name = "Printer", Price = 350.00, Category = c3 },
-                new Product() { Id = 9, Name = "MacBook", Price = 1800.00, Category = c2 },
-                new Product() { Id = 10, Name = "Sound Bar", Price = 700.00, Category = c3 },
-                new Product() { Id = 11, Name = "Level", Price = 70.00, Category = c1 }
-            };
+            List<Employee> employees = new List<Employee>();
 
-            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
-            var r1 =
-                from p in products
-                where p.Category.Tier == 1 && p.Price < 900.00
-                select p;
-            Print("TIER 1 AND PRICE < 900:", r1);
-
-            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
-            var r2 =
-                from p in products
-                where p.Category.Name == "Tools"
-                select p.Name;
-            Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
-
-            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
-            var r3 =
-                from p in products
-                where p.Name[0] == 'C'
-                select new
-                {
-                    p.Name,
-                    p.Price,
-                    CategoryName = p.Category.Name
-                };
-            Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECTS:", r3);
-
-            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
-            var r4 =
-                from p in products
-                where p.Category.Tier == 1
-                orderby p.Name
-                orderby p.Price // Ordem inversa de como quer ordenar
-                select p;
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
-
-            //var r5 = r4.Skip(2).Take(4);
-            var r5 =
-                (from p in r4
-                 select p)
-                 .Skip(2)
-                 .Take(4);
-            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
-
-            //var r6 = products.FirstOrDefault();
-            var r6 =
-                (from p in products
-                 select p)
-                 .FirstOrDefault();
-            Console.WriteLine("FirstOrDefault test1: " + r6);
-
-            //var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
-            var r7 =
-                (from p in products
-                 where p.Price > 3000.00
-                 select p)
-                 .FirstOrDefault();
-            Console.WriteLine("FirstOrDefault test2: ", r7 + "\n");
-
-            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-            Console.WriteLine("\nSingleOrDefault test1: " + r8);
-
-            var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-            Console.WriteLine("SingleOrDefault test2: " + r9 + "\n");
-
-            var r10 = products.Max(p => p.Price);
-            Console.WriteLine("Max price: " + r10);
-
-            var r11 = products.Min(p => p.Price);
-            Console.WriteLine("Min price: " + r11 + "\n");
-
-            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-            Console.WriteLine("Category 1 Sum Prices: " + r12);
-
-            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-            Console.WriteLine("Category 1 Average Prices: " + r13);
-
-            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty().Average();
-            Console.WriteLine("Category 5 Average Prices: " + r14);
-
-            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-            Console.WriteLine("Category 1 Aggregate sum: " + r15 + "\n");
-
-            //var r16 = products.GroupBy(p => p.Category);
-            var r16 =
-                from p in products
-                group p by p.Category;
-            foreach (IGrouping<Category, Product> group in r16)
+            try
             {
-                Console.WriteLine("Category " + group.Key.Name + ":");
-                foreach (Product p in group)
+                using (StreamReader sr = File.OpenText(filePath))
                 {
-                    Console.WriteLine(p);
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salaryValue = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, email, salaryValue));
+                    }
                 }
-                Console.WriteLine();
+
+                Console.Write("Enter salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Email of people whose salary is more than " + salary.ToString("F2", CultureInfo.InvariantCulture) + ":");
+                var emails = employees.Where(e => e.Salary > salary).OrderBy(e => e.Name).Select(e => e.Email);
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                Console.Write("Sum of salary of people whose name starts with 'M': ");
+                var sum = employees.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+                Console.WriteLine(sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
         }
     }
