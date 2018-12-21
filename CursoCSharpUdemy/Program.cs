dec01afb894aa484c98db3545204cf6b4e42567d
Henrique Miraldo <rickmiraldo@gmail.com>
2018-12-21 14:31:08 +0000

    LINQ com notação estilo SQL

--- CursoCSharpUdemy/Program.cs
+++ CursoCSharpUdemy/Program.cs
@@ -39,25 +39,62 @@ namespace CursoCSharpUdemy
                 new Product() { Id = 11, Name = "Level", Price = 70.00, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.00
+                select p;
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECTS:", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price // Ordem inversa de como quer ordenar
+                select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p)
+                 .Skip(2)
+                 .Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 =
+                (from p in products
+                 select p)
+                 .FirstOrDefault();
             Console.WriteLine("FirstOrDefault test1: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            var r7 =
+                (from p in products
+                 where p.Price > 3000.00
+                 select p)
+                 .FirstOrDefault();
             Console.WriteLine("FirstOrDefault test2: ", r7 + "\n");
 
             var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
@@ -84,7 +121,10 @@ namespace CursoCSharpUdemy
             var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
             Console.WriteLine("Category 1 Aggregate sum: " + r15 + "\n");
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 =
+                from p in products
+                group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
